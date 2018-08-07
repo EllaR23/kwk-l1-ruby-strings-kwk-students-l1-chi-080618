6ea66dd63fc5a5ad8f9e1b4d0f2cfd3f966795a0
EllaR23 <ellaratarac@gmail.com>
2018-08-07 14:16:06 +0000

    automatically backed up by learn

---
+++ invitation_customization.rb
@@ -0,0 +1,25 @@
+puts "What is the guest's name?"
+#{guest_name}gets.chomp
+
+puts "What is the name of the party or what type of party is it?"
+#{party_name}gets.chomp
+
+puts "What date is the party?"
+#{party_date}gets.chomp
+
+puts "What time does the party start?"
+#{party_time}gets.chomp
+
+puts ""
+
+puts "What is your name, the host of the party?"
+#{host_name}gets.chomp
+
+
+puts "Dear #{guest_name},"
+
+You are cordially invited to the Best #{party_name} Ever on #{party_date} at #{party_time}. Please RSVP no later than #{} .
+
+Sincerely,
+
+#{}

03e58bb7cba095e479107e96b5fcfc11ec149304
EllaR23 <ellaratarac@gmail.com>
2018-08-07 14:38:21 +0000

    automatically backed up by learn

--- invitation_customization.rb
+++ invitation_customization.rb
@@ -1,19 +1,25 @@
 puts "What is the guest's name?"
+name = gets.chomp
 #{guest_name}gets.chomp
 
 puts "What is the name of the party or what type of party is it?"
+name = gets.chomp
 #{party_name}gets.chomp
 
 puts "What date is the party?"
+date = gets.chomp
 #{party_date}gets.chomp
 
 puts "What time does the party start?"
+time = gets.chomp
 #{party_time}gets.chomp
 
 puts "What is the latest date a guest should RSVP?"
+date = gets.chomp
 #{RSVP_date}gets.chomp
 
 puts "What is your name, the host of the party?"
+name = gets.chomp
 #{host_name}gets.chomp
 
 
